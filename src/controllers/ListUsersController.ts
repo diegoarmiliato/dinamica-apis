55952b1c492a669f2414108aa19b550031b55771
Diego Armiliato <diegoarmiliato@gmail.com>
2020-10-26 23:07:01 -0300

    Enhanced ListUser api to export user and organization unit

--- src/controllers/ListUsersController.ts
+++ src/controllers/ListUsersController.ts
@@ -1,11 +1,10 @@
-import { Getuser } from './../utils/ldap/GetUser';
+import { Getuser } from '../utils/ldap/ListUser';
 import { Request, Response, NextFunction } from 'express';
 
 export const listUsers = async (req: Request, res: Response, next: NextFunction) : Promise<void> => {
   try {
     if (req.session.active) {
-      const {username} = req.body;
-      const users = await Getuser(username);
+      const users = await Getuser();
       res.json(users);
     } else {
       res.json({
